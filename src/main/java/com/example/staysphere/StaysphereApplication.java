fe759fabfd28bb6d141bfb1c37324116403c0980
Tiwari, Prashant <prashant@POC262.onmicrosoft.com>
2026-01-19 13:49:26 +0530

    Initial commit

---
+++ src/main/java/com/example/staysphere/StaysphereApplication.java
@@ -0,0 +1,13 @@
+package com.example.staysphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaysphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaysphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/staysphere/StaysphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.staysphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaysphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
